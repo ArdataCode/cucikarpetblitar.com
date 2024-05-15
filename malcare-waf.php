cc3d43e130ee1f60b1ffb3fba8cd8bfe70fd6db1
ArdataCode <ardata.indonesia@gmail.com>
2024-05-15 15:59:22 +0700

    Initial commit

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/u1571057/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/u1571057/public_html/wp-content/mc_data/');
+	define("MCCONFKEY", '5bfbfd36f5b1cb8b53a91070b1fc3806');
+	include_once('/home/u1571057/public_html/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
